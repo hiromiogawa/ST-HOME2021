158c40c499088fe32d8711d752900ae980798411
hiromi <hiromi.ogawa.1223@gmail.com>
2021-12-12 22:25:28 +0900

    カテゴリー絞り込み

--- src/components/pages/record/index.tsx
+++ src/components/pages/record/index.tsx
@@ -1,16 +1,16 @@
 import { FC, useState, memo, useEffect } from 'react';
-import { Link, useParams, useLocation } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
 import { Scontents } from '../../../styleSetting/CommonStyle';
 
 
-type categoryDataType = {
+type categoriesDataype = {
     id: number,
     name: string,
     slug: string
 }
 
-type recordListDataType = {
+type postsDataType = {
     id: number,
     title: {
         rendered: string
@@ -27,31 +27,58 @@ type recordListDataType = {
 }
 
 type urlParams = {
-    page: string
+    page: string,
+    category: string
 }
 
 export const RecordList: FC = memo(() => {
 
-    const [ categoryData, setCategoryData ] = useState<categoryDataType[]>([]);
-    const [ recordData, setRecordData ] = useState<recordListDataType[]>([]);
-    const { page } = useParams<urlParams>();
+    const [ categoriesData, setCategoryData ] = useState<categoriesDataype[]>([]);
+    const [ apiUrl, setApiUrl ] = useState<string>();
+    const [ postsData, setPostsData ] = useState<postsDataType[]>([]);
+    const { page, category } = useParams<urlParams>();
+
+    const getJson = (url: string, type: string) => {
+        axios.get(url).then((res) => {
+            switch(type) {
+                case 'posts':
+                setPostsData(res.data);
+                break;
+                case 'categories':
+                setCategoryData(res.data);
+                break;
+            }
+        })
+    }
         
+    useEffect(() => {
+        getJson('http://wp.hiromi-ogawa.com/wp-json/wp/v2/categories', 'categories');
+    }, []);
 
     useEffect(() => {
-        axios.get(`http://wp.hiromi-ogawa.com/wp-json/wp/v2/categories`).then((categoryRes) => {
-            setCategoryData(categoryRes.data);
-        })
-        axios.get(`http://wp.hiromi-ogawa.com/wp-json/wp/v2/posts?_embed&per_page=8&page=${page}`).then((recordRes) => {
-            setRecordData(recordRes.data);
-        })
-    }, []);
-    const categoryList = categoryData.map((value,i) => (
+        if(categoriesData) {
+            const result = categoriesData.find((value) => value.slug === category);
+            if(result) {
+                setApiUrl(`http://wp.hiromi-ogawa.com/wp-json/wp/v2/posts?_embed&per_page=12&page=${page}&categories=${result.id}`);
+            } else if (category == 'all') {
+                setApiUrl(`http://wp.hiromi-ogawa.com/wp-json/wp/v2/posts?_embed&per_page=12&page=${page}`);
+            }
+        }
+    }, [categoriesData, category, page]);
+
+    useEffect(() => {
+        if (apiUrl) {
+            getJson(apiUrl, 'posts');
+        }
+    }, [apiUrl]);
+
+    const categoryList = categoriesData.map((value,i) => (
         <li key={i}>
             <Link to={`/record/${value.slug}/1`}>{value.name}</Link>
         </li>
     ));
 
-    const recordList = recordData.map((value,i) => (
+    const recordList = postsData.map((value,i) => (
         <li key={i}>
             <article>
             <Link to={`/record/detail/${value.id}`}>
@@ -59,7 +86,6 @@ export const RecordList: FC = memo(() => {
                 <div className="txt-area">
                     <h2 className="title">{value.title.rendered}</h2>
                 </div>
-                <p>{value.category_name[1]}</p>
             </Link>
             <ul className="category">
                 {value.category_name.map((cat, i, []) =>
