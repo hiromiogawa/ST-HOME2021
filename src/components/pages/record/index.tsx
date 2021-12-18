9e195fa7a92c726ef17929717557fa2139050d47
hiromi <hiromi.ogawa.1223@gmail.com>
2021-12-18 23:38:48 +0900

    pagenavi作成

--- src/components/pages/record/index.tsx
+++ src/components/pages/record/index.tsx
@@ -31,21 +31,24 @@ type urlParams = {
     category: string
 }
 
-export const RecordList: FC = memo(() => {
 
+export const RecordList: FC = memo(() => {
     const [ categoriesData, setCategoryData ] = useState<categoriesDataype[]>([]);
+    const [ totalPage, setTotalpage] = useState<number>(1);
     const [ apiUrl, setApiUrl ] = useState<string>();
     const [ postsData, setPostsData ] = useState<postsDataType[]>([]);
     const { page, category } = useParams<urlParams>();
+    const perPage: number = 1;
 
     const getJson = (url: string, type: string) => {
         axios.get(url).then((res) => {
             switch(type) {
                 case 'posts':
-                setPostsData(res.data);
+                    setTotalpage(Number(res.headers["x-wp-totalpages"]));
+                    setPostsData(res.data);
                 break;
                 case 'categories':
-                setCategoryData(res.data);
+                    setCategoryData(res.data);
                 break;
             }
         })
@@ -59,9 +62,9 @@ export const RecordList: FC = memo(() => {
         if(categoriesData) {
             const result = categoriesData.find((value) => value.slug === category);
             if(result) {
-                setApiUrl(`http://wp.hiromi-ogawa.com/wp-json/wp/v2/posts?_embed&per_page=12&page=${page}&categories=${result.id}`);
+                setApiUrl(`http://wp.hiromi-ogawa.com/wp-json/wp/v2/posts?_embed&per_page=${perPage}&page=${page}&categories=${result.id}`);
             } else if (category == 'all') {
-                setApiUrl(`http://wp.hiromi-ogawa.com/wp-json/wp/v2/posts?_embed&per_page=12&page=${page}`);
+                setApiUrl(`http://wp.hiromi-ogawa.com/wp-json/wp/v2/posts?_embed&per_page=${perPage}&page=${page}`);
             }
         }
     }, [categoriesData, category, page]);
@@ -89,20 +92,72 @@ export const RecordList: FC = memo(() => {
             </Link>
             <ul className="category">
                 {value.category_name.map((cat, i, []) =>
-                <li key={i}><Link to={"/record/1?categories=" + value.categories[i]}>{cat}</Link></li>
+                <li key={i}><Link to={`/record/?categories=${value.categories[i]}`}>{cat}</Link></li>
                 )}
             </ul>
             </article>
         </li>
     ));
 
+    const getPageNation = (totalPage: number) => {
+        let startPage: number;
+        let endPage: number;
+        const numberPage: number = Number(page);
+        const pageNation: JSX.Element[]  = [];
+
+        if(numberPage + 3 > totalPage) {
+            endPage = totalPage;
+        } else {
+            endPage = numberPage + 2;
+        }
+
+        if(numberPage < 3 ) {
+            startPage = 1;
+        } else if(numberPage + 2 > totalPage) {
+            startPage = numberPage - ((numberPage + 4) - totalPage);
+        } else {
+            startPage = numberPage - 2;
+        }
+
+        if(endPage < 5 ) {
+            startPage = 1;
+            if(numberPage < 3) {
+                endPage = endPage + ((numberPage - 3) * -1);
+                if (endPage > totalPage) {
+                    endPage = totalPage;
+                }
+            }
+        }
+
+        for(let i = startPage; i <= endPage; i++) {
+            if (numberPage == i) {
+                pageNation.push(
+                    <li key={i}>{i}</li>
+                )
+            } else {
+                pageNation.push(
+                    <li key={i}><Link to={`/record/${category}/${i}`}>{i}</Link></li>
+                )            
+            }
+        }
+
+        return pageNation;
+    };
 
     return (
         <article>
             <div className="page_heading"></div>
             <Scontents>
-                { categoryList }
-                { recordList }
+                <ul>
+                <li><Link to="/record/all/1">全て</Link></li>
+                    { categoryList }
+                </ul>
+                <ul>
+                    { recordList }
+                </ul>
+                <ol>
+                    { getPageNation(totalPage) }
+                </ol>
             </Scontents>
         </article>
     );
